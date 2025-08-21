7342354ffe78094a6e17a0d174a565c5b37dd29b
Joshtri <stuffofyos1516@gmail.com>
2025-08-21 13:41:04 +0700

    Commit: index.ts

---
+++ types/index.ts
@@ -0,0 +1,34 @@
+import { SVGProps } from "react";
+import { SimpleIcon } from "simple-icons";
+
+export type IconSvgProps = SVGProps<SVGSVGElement> & {
+  size?: number;
+};
+
+export type SocialKeys =
+  | "github"
+  | "linkedin"
+  | "x"
+  | "instagram"
+  | "facebook"
+  | "dribbble"
+  | "behance"
+  | "youtube"
+  | "website"
+  | "portfolio";
+
+export type TechItem =
+  | { name: string; icon: SimpleIcon }   // untuk simple-icons
+  | { name: string; local: string }      // untuk file lokal SVG
+  | { name: string; initials: string };  // untuk fallback monogram
+
+
+export type TeamMember = {
+  name: string;
+  role: string;
+  description: string;
+  skills: string[];
+  image?: string;
+  /** tambahkan link sosial/portofolio di sini (opsional per kunci) */
+  socials?: Partial<Record<SocialKeys, string>>;
+};
