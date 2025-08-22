5e24414700a7e5ff8ff566f33cab4e2de5335ce9
Joshtri <stuffofyos1516@gmail.com>
2025-08-22 21:31:21 +0700

    Commit: Footer.tsx

--- components/partials/Footer.tsx
+++ components/partials/Footer.tsx
@@ -3,7 +3,7 @@
 
 export default function Footer() {
   return (
-    <footer className="bg-surface py-13 text-center text-sm text-white">
+    <footer className="bg-surface py-12 text-center text-lg text-white">
       <div className="container mx-auto px-4">
         <p>&copy; {new Date().getFullYear()} Ngodingin. All rights reserved.</p>
       </div>
