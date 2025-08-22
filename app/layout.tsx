42f49c9d976d0007bb7a367963d65e6551b1bcc0
Joshtri <stuffofyos1516@gmail.com>
2025-08-22 21:31:19 +0700

    Commit: layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ import CustomNavbar from "@/components/partials/Navbar";
 import { fontSans } from "@/config/fonts";
 // import { Navbar } from "@/components/partials/Navbar";
 import FloatingScrollToTop from "@/components/common/FloatingScrollToTop";
+import FloatingWhatsAppButton from "@/components/common/FloatingWhatsAppButton";
 
 export const metadata: Metadata = {
   title: "Ngodingin",
@@ -45,6 +46,27 @@ export default function RootLayout({
           <div className="relative flex flex-col min-h-screen">
             <CustomNavbar />
             <FloatingScrollToTop /> {/* muncul di semua page */}
+            <FloatingWhatsAppButton
+              defaultContact={{
+                name: "Admin Ngodingin",
+                number: "62812345678",
+                prefilledText:
+                  "Halo! Saya tertarik untuk konsultasi terkait tugas akhir.",
+              }}
+              contacts={[
+                {
+                  name: "Admin Ngodingin",
+                  number: "62812345678",
+                  prefilledText:
+                    "Halo! Saya tertarik untuk konsultasi terkait tugas akhir.",
+                },
+                {
+                  name: "Technical Support",
+                  number: "62887654321",
+                  prefilledText: "Halo! Saya butuh bantuan teknis.",
+                },
+              ]}
+            />
             <main className="flex-grow pt-16">{children}</main>
             <Footer />
           </div>
